f005afd49ace96522b431a3eb603d0bf305bcd0d
Crowdbotics <team@crowdbotics.com>
2024-01-25 20:53:50 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "muddy-bird-45551",
+  url: "https://muddy-bird-45551.botics.co",
+  api: "https://muddy-bird-45551.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
